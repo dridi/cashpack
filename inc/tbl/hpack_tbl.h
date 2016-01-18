489a0d296ac177f7fc27eb4c3b004a63cc2123a0
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2016-01-18 08:42:01 +0100

    Generate hpack.h from tables
    In order to make enums reusable in other places like documentation
    it is generated at build time. It allows to ship a single header file
    to end users, making things cleaner.
    
    If available, the file is formatted using astyle(1).

---
+++ inc/tbl/hpack_tbl.h
@@ -0,0 +1,47 @@
+/*-
+ * Copyright (c) 2016 Dridi Boukelmoune
+ * All rights reserved.
+ *
+ * Author: Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifdef HPR
+HPR(OK,   0, "Success")
+HPR(ARG, -1, "Wrong argument")
+HPR(BUF, -2, "Buffer overflow")
+HPR(INT, -3, "Integer overflow")
+HPR(IDX, -4, "Invalid index")
+HPR(LEN, -5, "Invalid length")
+HPR(HUF, -6, "Invalid Huffman code")
+#endif
+
+#ifdef HPE
+HPE(FIELD, 0, "New field")
+HPE(NEVER, 1, "Field is never indexed")
+HPE(INDEX, 2, "Field was indexed")
+HPE(NAME,  3, "New name string")
+HPE(VALUE, 4, "New Value string")
+HPE(DATA,  5, "String data")
+HPE(TABLE, 6, "Table update")
+#endif
