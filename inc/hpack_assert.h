17b541421869fe85ec0c4eae83d00cdc3b2e35bb
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2016-12-23 18:51:21 +0100

    WRONG should always lead to a crash

--- inc/hpack_assert.h
+++ inc/hpack_assert.h
@@ -4,9 +4,16 @@
  * This file is in the public domain.
  */
 
+#ifdef NDEBUG
+#define WRONG(str)		\
+	do {			\
+		abort();	\
+	} while (0)
+#else
 #define WRONG(str)		\
 	do {			\
 		assert(!str);	\
 	} while (0)
+#endif
 
 #define INCOMPL()	WRONG("Incomplete code")
