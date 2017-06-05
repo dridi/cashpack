cacc347205e8fdc7e0c85355a188344673cf588a
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2017-06-05 15:04:55 +0200

    Build hpack.h also on FreeBSD
    The wouldn't work because of `sed -i` (while amazingly it worked on
    MacOS) and possibly for other reasons. It was possible to build a dist
    archive on FreeBSD, now it builds from git too.
    
    That also makes the script slightly simpler to follow.

--- inc/format_header.sh
+++ inc/format_header.sh
@@ -25,56 +25,52 @@
 # SUCH DAMAGE.
 
 set -e
+set -u
 
-TMP_C_FILE=$(mktemp tmp.XXXXXXXXXX.c)
-TMP_H_FILE=$(mktemp tmp.XXXXXXXXXX.h)
-SRC_C_FILE=$(mktemp src.XXXXXXXXXX.c)
-SRC_H_FILE=$(mktemp src.XXXXXXXXXX.h)
-
+TMP_DIR=$(mktemp -d tmp.XXXXXX)
 SRC_DIR=$(dirname "$1")
 
-trap 'rm -f "$TMP_C_FILE" "$TMP_H_FILE" "$SRC_C_FILE" "$SRC_H_FILE"' EXIT
+trap 'rm -rf "$TMP_DIR"' EXIT
 
 # Usage: ./$0 source.h.in target.h
 test $# = 2
 test -f "$1"
 
 # Create a dummy C file to detect potential junk
-cat >"$TMP_C_FILE" <<EOF
+cat >"$TMP_DIR/dummy.c" <<EOF
 const int END_OF_HEADER = 0;
 EOF
 
 # Append the dummy C code to the source header
-cat "$1" "$TMP_C_FILE" >"$SRC_C_FILE"
+cat "$1" "$TMP_DIR/dummy.c" >"$TMP_DIR/dummy_hpack.c"
 
 # Process the headers as C files
-cpp -C -P "$TMP_C_FILE" "$TMP_H_FILE"
-cpp -C -P -I"$SRC_DIR" "$SRC_C_FILE" "$SRC_H_FILE"
+cpp -C -P "$TMP_DIR/dummy.c" >"$TMP_DIR/dummy.h"
+cpp -C -P -I"$SRC_DIR" "$TMP_DIR/dummy_hpack.c" >"$TMP_DIR/dummy_hpack.h"
 
 # Remove protections
-sed -i -e /REMOVE_ME/d "$SRC_H_FILE"
+sed -e /REMOVE_ME/d <"$TMP_DIR/dummy_hpack.h" >"$TMP_DIR/junk_hpack.h"
 
 # Measure prepended junkness
-JUNK=$(sed -e /END_OF_HEADER/q <"$TMP_H_FILE" | wc -l | awk '{print $1}')
+JUNK=$(sed -e /END_OF_HEADER/q <"$TMP_DIR/dummy.h" | wc -l | awk '{print $1}')
 
 # Remove potential junk prepended by the preprocessor.
-# The temp header is no longer needed as is, reuse it.
 # The temp C source contains a single line of code to
 # help the `-n` option cope with a lack of junk.
-tail -n "+$JUNK" <"$SRC_H_FILE" >"$TMP_H_FILE"
+tail -n "+$JUNK" <"$TMP_DIR/junk_hpack.h" >"$TMP_DIR/eoh_hpack.h"
 
 # Remove the dummy C code
-sed -i -e /END_OF_HEADER/d "$TMP_H_FILE"
+sed -e /END_OF_HEADER/d <"$TMP_DIR/eoh_hpack.h" >"$TMP_DIR/osx_hpack.h"
 
 # Work around weird macro expansion on OSX
-sed -i -e s/##// "$TMP_H_FILE"
+sed -e s/##// <"$TMP_DIR/osx_hpack.h" >"$TMP_DIR/hpack.h"
 
 # Make it look less bad
 if command -v uncrustify >/dev/null
 then
 	CFG=$SRC_DIR/uncrustify.cfg
-	uncrustify -c "$CFG" -q -l C --no-backup "$TMP_H_FILE"
+	uncrustify -c "$CFG" -q -l C --no-backup "$TMP_DIR/hpack.h"
 fi
 
 # And voilà
-cp -f "$TMP_H_FILE" "$2"
+cp -f "$TMP_DIR/hpack.h" "$2"
