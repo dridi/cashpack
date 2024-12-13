e5c782a522392aaa83530dd4cd661d409d2747a3
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2024-12-13 17:01:25 +0100

    Add test coverage for manual pages synopsis

---
+++ tst/hpack_dep.c
@@ -0,0 +1,20 @@
+/*-
+ * Written by Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
+ *
+ * This file is in the public domain.
+ *
+ * Check header dependencies advertised in manual pages.
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "hpack.h"
+
+int
+main(void)
+{
+
+	return (0);
+}
